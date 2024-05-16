83359329222dd143b31146adca66e972d42cdbe6
cosmin-marginean <cos@hey.com>
2024-05-16 20:13:17 +0100

    Map function for RecordsPage

--- invirt-data/src/main/kotlin/invirt/data/RecordsPage.kt
+++ invirt-data/src/main/kotlin/invirt/data/RecordsPage.kt
@@ -8,6 +8,10 @@ data class RecordsPage<T : Any>(
 ) {
     val pagination = Pagination(page, count)
 
+    fun <R : Any> map(map: (T) -> R): RecordsPage<R> {
+        return RecordsPage(records = records.map(map), count, page, sort)
+    }
+
     companion object {
         fun <T : Any> empty() = RecordsPage<T>(emptyList(), 0, Page(0, 1))
     }
--- invirt-data/src/test/kotlin/invirt/data/RecordsPageTest.kt
+++ invirt-data/src/test/kotlin/invirt/data/RecordsPageTest.kt
@@ -12,4 +12,18 @@ class RecordsPageTest : StringSpec({
         recordsPage.count shouldBe 0
         recordsPage.sort shouldBe emptyList()
     }
+
+    "map" {
+        RecordsPage(
+            records = listOf("one", "two", "three", "four"),
+            count = 12432,
+            page = Page(0, 10),
+            sort = listOf(Sort.asc("name"))
+        ).map { it.length } shouldBe RecordsPage(
+            records = listOf(3, 3, 5, 4),
+            count = 12432,
+            page = Page(0, 10),
+            sort = listOf(Sort.asc("name"))
+        )
+    }
 })
