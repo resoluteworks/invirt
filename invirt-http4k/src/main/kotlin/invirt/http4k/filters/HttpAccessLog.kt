30984aa28ca5d2a7f7fd5e4010d140449ef8d5df
cosmin-marginean <cos@hey.com>
2024-06-09 08:10:37 +0100

    Exclude headers in http access log

--- invirt-http4k/src/main/kotlin/invirt/http4k/filters/HttpAccessLog.kt
+++ invirt-http4k/src/main/kotlin/invirt/http4k/filters/HttpAccessLog.kt
@@ -3,6 +3,7 @@ package invirt.http4k.filters
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.http4k.core.Filter
 import org.http4k.core.HttpTransaction
+import org.http4k.core.toParametersMap
 import org.http4k.filter.ResponseFilters
 
 object HttpAccessLog {
@@ -12,8 +13,10 @@ object HttpAccessLog {
     operator fun invoke(
         allStatues: Boolean = false,
         ignorePaths: Set<String> = emptySet(),
+        excludeHeaders: Set<String> = emptySet(),
         extraFields: (HttpTransaction) -> Map<String, String> = { emptyMap() }
     ): Filter {
+        val lowerCaseExcludedHeaders = excludeHeaders.map { it.lowercase() }
         return ResponseFilters.ReportHttpTransaction(recordFn = { tx ->
             if ((allStatues || tx.response.status.code >= 400) && ignorePaths.none { tx.request.uri.path.startsWith(it) }) {
                 log.atInfo {
@@ -24,7 +27,10 @@ object HttpAccessLog {
                         "path" to tx.request.uri.path,
                         "uri" to tx.request.uri.toString(),
                         "status" to tx.response.status.code,
-                        "durationMs" to tx.duration.toMillis()
+                        "durationMs" to tx.duration.toMillis(),
+                        "headers" to tx.request.headers
+                            .filter { it.first.lowercase() !in lowerCaseExcludedHeaders }
+                            .toParametersMap()
                     ).plus(extraFields(tx))
                 }
             }
