c2d5450aff9ccb545e86ead8c66addcf506c3f26
cosmin-marginean <cos@hey.com>
2024-06-10 19:48:43 +0100

    Add bbox array support

--- invirt-data/src/main/kotlin/invirt/data/geo/GeoBoundingBox.kt
+++ invirt-data/src/main/kotlin/invirt/data/geo/GeoBoundingBox.kt
@@ -14,4 +14,19 @@ data class GeoBoundingBox(
         northEast,
         GeoLocation(northEast.lng, southWest.lat)
     )
+
+    companion object {
+        operator fun invoke(bbox: List<Double>): GeoBoundingBox {
+            if (bbox.size != 4) {
+                throw IllegalArgumentException("Bounding box coordinates array must be of size 4")
+            }
+            return GeoBoundingBox(
+                southWest = GeoLocation(lng = bbox[0], lat = bbox[1]),
+                northEast = GeoLocation(lng = bbox[2], lat = bbox[3])
+            )
+        }
+
+        operator fun invoke(vararg bbox: Double): GeoBoundingBox = GeoBoundingBox(bbox.toList())
+        operator fun invoke(bbox: String): GeoBoundingBox = GeoBoundingBox(bbox.split(",").map { it.toDouble() })
+    }
 }
--- invirt-data/src/test/kotlin/invirt/data/GeoBoundingBoxTest.kt
+++ invirt-data/src/test/kotlin/invirt/data/GeoBoundingBoxTest.kt
@@ -2,6 +2,7 @@ package invirt.data
 
 import invirt.data.geo.GeoBoundingBox
 import invirt.data.geo.GeoLocation
+import io.kotest.assertions.throwables.shouldThrowWithMessage
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
@@ -14,6 +15,32 @@ class GeoBoundingBoxTest : StringSpec({
             GeoLocation(lng = -8.596867, lat = 56.435911),
             GeoLocation(lng = 1.950008, lat = 56.435911),
             GeoLocation(lng = 1.950008, lat = 51.348611)
+        )
+    }
+
+    "from array/list" {
+        GeoBoundingBox(-8.596867, 51.348611, 1.950008, 56.435911) shouldBe GeoBoundingBox(
+            GeoLocation(lng = -8.596867, lat = 51.348611),
+            GeoLocation(lng = 1.950008, lat = 56.435911)
+        )
+    }
+
+    "from array/list - invalid args" {
+        shouldThrowWithMessage<IllegalArgumentException>("Bounding box coordinates array must be of size 4") {
+            GeoBoundingBox()
+        }
+        shouldThrowWithMessage<IllegalArgumentException>("Bounding box coordinates array must be of size 4") {
+            GeoBoundingBox(-8.596867)
+        }
+        shouldThrowWithMessage<IllegalArgumentException>("Bounding box coordinates array must be of size 4") {
+            GeoBoundingBox(-8.596867, 51.348611, 1.950008, 56.435911, 56.435911)
+        }
+    }
+
+    "from string" {
+        GeoBoundingBox("-8.596867,51.348611,1.950008,56.435911") shouldBe GeoBoundingBox(
+            GeoLocation(lng = -8.596867, lat = 51.348611),
+            GeoLocation(lng = 1.950008, lat = 56.435911)
         )
     }
 })
