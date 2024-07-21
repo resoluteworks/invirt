1b2d4e03a79d722b96916bdb2926bca6adcab89d
cosmin-marginean <cos@hey.com>
2024-07-21 17:32:29 +0100

    Docs

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -4,40 +4,42 @@ import styles from './styles.module.css';
 
 const FeatureList = [
     {
-        title: 'Easy to Use',
+        title: 'Making the most of Kotlin',
         Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
         description: (
             <>
-                Docusaurus was designed from the ground up to be easily installed and
-                used to get your website up and running quickly.
+                Invirt is build in entirely in Kotlin (JVM) and makes the most of
+                the language capabilities to deliver fluent and type-safe approaches
+                to building web applications.
             </>
         ),
     },
     {
-        title: 'Focus on What Matters',
+        title: 'Developer experience as a key goal',
         Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
         description: (
             <>
-                Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-                ahead and move your docs into the <code>docs</code> directory.
+                Invirt provides a series of tools and utilities to make bootstrapping
+                your application as easy and quick as possible, and your development process
+                frictionless.
             </>
         ),
     },
-    {
-        title: 'Powered by React',
-        Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-        description: (
-            <>
-                Extend or customize your website layout by reusing React. Docusaurus can
-                be extended while reusing the same header and footer.
-            </>
-        ),
-    },
+    // {
+    //     title: 'Powered by React',
+    //     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    //     description: (
+    //         <>
+    //             Extend or customize your website layout by reusing React. Docusaurus can
+    //             be extended while reusing the same header and footer.
+    //         </>
+    //     ),
+    // },
 ];
 
 function Feature({Svg, title, description}) {
     return (
-        <div className={clsx('col col--4')}>
+        <div className={clsx('col')}>
             <div className="text--center">
                 <Svg className={styles.featureSvg} role="img"/>
             </div>
