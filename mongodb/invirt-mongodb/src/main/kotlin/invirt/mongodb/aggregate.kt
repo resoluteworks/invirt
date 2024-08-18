51b4759ef85b1f22e893e0fd43562a4483c97692
cosmin-marginean <cos@hey.com>
2024-08-18 17:39:36 +0100

    Add paged aggregate util

---
+++ mongodb/invirt-mongodb/src/main/kotlin/invirt/mongodb/aggregate.kt
@@ -0,0 +1,70 @@
+package invirt.mongodb
+
+import com.mongodb.client.model.Aggregates
+import com.mongodb.client.model.CountOptions
+import com.mongodb.client.model.Facet
+import com.mongodb.client.model.Filters
+import com.mongodb.client.model.Sorts
+import com.mongodb.kotlin.client.MongoCollection
+import invirt.data.Page
+import invirt.data.RecordsPage
+import org.bson.Document
+import org.bson.conversions.Bson
+
+fun <Doc : Any> MongoCollection<Doc>.query(
+    filter: Bson? = null,
+    page: Page = Page(0, 10),
+    maxDocuments: Int = 1000,
+    sort: List<Bson> = emptyList()
+): RecordsPage<Doc> {
+    val docFilter = filter ?: Filters.empty()
+
+    var find = this.find(docFilter)
+    if (sort.isNotEmpty()) {
+        find = find.sort(Sorts.orderBy(sort))
+    }
+
+    return RecordsPage(
+        records = find.page(page).toList(),
+        totalCount = this.countDocuments(docFilter, CountOptions().limit(maxDocuments)),
+        page = page
+    )
+}
+
+/**
+ * Runs a [MongoCollection.aggregate] for the specified [pipeline] with the given [page]
+ * and optionally additional [facets]. Returns a [PagedAggregateSearchResult] containing
+ * the [RecordsPage] for the specified page and the raw response document.
+ */
+fun <Doc : Any> MongoCollection<Doc>.pagedAggregate(
+    pipeline: List<Bson>,
+    page: Page,
+    facets: List<Facet> = emptyList()
+): PagedAggregateSearchResult<Doc> {
+    val allFacets = listOf(
+        Facet("documents", Aggregates.skip(page.from), Aggregates.limit(page.size)),
+        Facet("totalCount", Aggregates.count())
+    ).plus(facets)
+
+    val results = withDocumentClass<Document>()
+        .aggregate(pipeline.plus(Aggregates.facet(allFacets)))
+        .toList()
+        .first()
+
+    val recordsPage = RecordsPage(
+        records = deserialize(results["documents"] as List<Document>),
+        totalCount = (results["totalCount"] as List<Document>).firstOrNull()?.getInteger("count")?.toLong() ?: 0L,
+        page = page
+    )
+
+    return PagedAggregateSearchResult(recordsPage, results)
+}
+
+/**
+ * Contains the result of a [MongoCollection.pagedAggregate] call as a [RecordsPage]
+ * and a [rawResult] with the original MongoDB aggregate response.
+ */
+class PagedAggregateSearchResult<Doc : Any>(
+    val recordsPage: RecordsPage<Doc>,
+    val rawResult: Document
+)
---
+++ mongodb/invirt-mongodb/src/test/kotlin/invirt/mongodb/AggregatesTest.kt
@@ -0,0 +1,86 @@
+package invirt.mongodb
+
+import com.mongodb.client.model.Aggregates
+import com.mongodb.client.model.Facet
+import invirt.data.Page
+import invirt.data.sortAsc
+import invirt.data.sortDesc
+import invirt.randomTestCollection
+import invirt.testMongo
+import invirt.utils.uuid7
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
+import org.bson.Document
+import org.bson.codecs.pojo.annotations.BsonId
+
+class AggregatesTest : StringSpec() {
+
+    private val mongo = testMongo()
+
+    init {
+
+        "paged aggregate - basics" {
+            data class TestDocument(
+                val index: Int,
+                @BsonId override val id: String = uuid7(),
+                override var version: Long = 0
+            ) : VersionedDocument
+
+            val collection = mongo.randomTestCollection<TestDocument>()
+
+            repeat(100) {
+                collection.insert(TestDocument(it + 1))
+            }
+
+            collection.pagedAggregate(
+                pipeline = listOf(
+                    Aggregates.match(TestDocument::index.mongoLte(50)),
+                    Aggregates.sort(TestDocument::index.sortAsc().mongoSort())
+                ),
+                page = Page(0, 10)
+            ).recordsPage.records.map { it.index } shouldContainExactly (1..10).toList()
+
+            collection.pagedAggregate(
+                pipeline = listOf(
+                    Aggregates.match(TestDocument::index.mongoLte(50)),
+                    Aggregates.sort(TestDocument::index.sortDesc().mongoSort())
+                ),
+                page = Page(0, 10)
+            ).recordsPage.records.map { it.index } shouldContainExactly (50 downTo 41).toList()
+        }
+
+        "paged aggregate - with custom facets" {
+            data class TestDocument(
+                val index: Int,
+                val type: String,
+                @BsonId override val id: String = uuid7(),
+                override var version: Long = 0
+            ) : VersionedDocument
+
+            val collection = mongo.randomTestCollection<TestDocument>()
+
+            repeat(100) {
+                val type = if (it % 2 == 0) "person" else "company"
+                collection.insert(TestDocument(it + 1, type))
+            }
+
+            val result = collection.pagedAggregate(
+                pipeline = listOf(
+                    Aggregates.match(TestDocument::index.mongoLte(50)),
+                    Aggregates.sort(TestDocument::index.sortAsc().mongoSort())
+                ),
+                page = Page(0, 10),
+                facets = listOf(
+                    Facet(
+                        "count-companies",
+                        listOf(Aggregates.match(TestDocument::type.mongoEq("company")), Aggregates.count())
+                    )
+                )
+            )
+
+            result.recordsPage.records.map { it.index } shouldContainExactly (1..10).toList()
+            (result.rawResult["count-companies"] as List<Document>)[0]["count"] shouldBe 25
+        }
+    }
+}
