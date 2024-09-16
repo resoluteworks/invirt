0dfc426bbc5c8302f2d02a06a32caa2e29279fb2
cosmin-marginean <cos@hey.com>
2024-09-16 19:16:17 +0100

    Mongo exception utility

---
+++ mongodb/invirt-mongodb/src/main/kotlin/invirt/mongodb/errors.kt
@@ -0,0 +1,12 @@
+package invirt.mongodb
+
+import com.mongodb.ErrorCategory
+import com.mongodb.MongoCommandException
+import com.mongodb.MongoException
+import com.mongodb.MongoWriteException
+
+private const val ERROR_CODE_DUPLICATE_KEY = 11000
+
+fun MongoException.isDuplicateError(): Boolean =
+    (this is MongoCommandException && this.errorCode == ERROR_CODE_DUPLICATE_KEY) ||
+        (this is MongoWriteException && this.error.category == ErrorCategory.DUPLICATE_KEY)
