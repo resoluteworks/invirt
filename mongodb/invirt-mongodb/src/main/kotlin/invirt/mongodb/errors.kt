3818de52b61b0e2d017b432a6b62dcbceb10529a
cosmin-marginean <cos@hey.com>
2024-09-16 21:36:24 +0100

    Code cleanup

--- mongodb/invirt-mongodb/src/main/kotlin/invirt/mongodb/errors.kt
+++ mongodb/invirt-mongodb/src/main/kotlin/invirt/mongodb/errors.kt
@@ -7,8 +7,5 @@ import com.mongodb.MongoWriteException
 
 private const val ERROR_CODE_DUPLICATE_KEY = 11000
 
-fun MongoException.isDuplicateError(): Boolean {
-    // sout
-    return (this is MongoCommandException && this.errorCode == ERROR_CODE_DUPLICATE_KEY) ||
-        (this is MongoWriteException && this.error.category == ErrorCategory.DUPLICATE_KEY)
-}
+fun MongoException.isDuplicateError(): Boolean = (this is MongoCommandException && this.errorCode == ERROR_CODE_DUPLICATE_KEY) ||
+    (this is MongoWriteException && this.error.category == ErrorCategory.DUPLICATE_KEY)
