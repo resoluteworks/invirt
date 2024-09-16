a24b1b41f28f7ece5434ee50d469cef93e4ffb12
cosmin-marginean <cos@hey.com>
2024-09-16 19:27:01 +0100

    Tests for mongo exception util

---
+++ mongodb/invirt-mongodb/src/test/kotlin/invirt/mongodb/ErrorsTest.kt
@@ -0,0 +1,39 @@
+package invirt.mongodb
+
+import com.mongodb.MongoException
+import invirt.randomTestCollection
+import invirt.testMongo
+import invirt.utils.uuid7
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+import org.bson.codecs.pojo.annotations.BsonId
+
+class ErrorsTest : StringSpec() {
+
+    private val mongo = testMongo()
+
+    init {
+        "duplicate errors" {
+            data class TestDocument(
+                @BsonId override val id: String,
+                override var version: Long = 0
+            ) : VersionedDocument
+
+            val collection = mongo.randomTestCollection<TestDocument>()
+            val id = uuid7()
+            collection.insertOne(TestDocument(id))
+            val exception = try {
+                collection.insertOne(TestDocument(id))
+                null
+            } catch (e: Exception) {
+                e
+            }
+
+            exception shouldNotBe null
+            exception.shouldBeInstanceOf<MongoException>()
+            exception.isDuplicateError() shouldBe true
+        }
+    }
+}
