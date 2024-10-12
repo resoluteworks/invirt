0936494a90f81c6626205f3b3758bd8381e70040
cosmin-marginean <cos@hey.com>
2024-10-12 19:19:04 +0100

    uuidBase32 utils

--- invirt-utils/build.gradle.kts
+++ invirt-utils/build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
 
 dependencies {
     implementation("com.github.f4b6a3:uuid-creator:5.3.7")
+    implementation("commons-codec:commons-codec:1.17.0")
 }
--- invirt-utils/src/main/kotlin/invirt/utils/id.kt
+++ invirt-utils/src/main/kotlin/invirt/utils/id.kt
@@ -1,9 +1,21 @@
 package invirt.utils
 
 import com.github.f4b6a3.uuid.UuidCreator
+import org.apache.commons.codec.binary.Base32
+import java.nio.ByteBuffer
+import java.util.*
 
-fun uuid7(): String {
-    return UuidCreator.getTimeOrderedEpoch()
-        .toString()
-        .replace("-", "")
+fun uuid7(): String = UuidCreator.getTimeOrderedEpoch()
+    .toString()
+    .replace("-", "")
+
+fun UUID.toByteArray(): ByteArray {
+    val bb: ByteBuffer = ByteBuffer.allocate(16)
+    bb.putLong(mostSignificantBits)
+    bb.putLong(leastSignificantBits)
+    return bb.array()
 }
+
+fun UUID.toBase32(): String = Base32().encodeAsString(toByteArray()).lowercase().replace("=", "")
+
+fun uuidBase32(): String = UUID.randomUUID().toBase32()
